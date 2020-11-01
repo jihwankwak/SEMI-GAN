b8b7de9a8c56bb329a53fab4a939dfca178a72e5
jihwankwak <jihwan0508@naver.com>
2020-11-02 01:25:06 +0900

    rdf

--- FINAL/sample_data/sample_utils.py
+++ FINAL/sample_data/sample_utils.py
@@ -57,63 +57,135 @@ def FID_score_each_X(generated_samples, real_samples, num_in_gen, num_in_cycle):
         FID_score_list.append(FID)
     return np.mean(np.array(FID_score_list)), FID_score_list
 
-def KL(P,Q):
-    epsilon = 1e-15
-    P = P+epsilon
-    Q = Q+epsilon
+def EMD_from_2d_kde_integral(generated_samples, real_samples, index_x, index_y, num_coordinate): #각 joint pair 별로 2d sample을 kde로 fittining시키고 얻은 pdf값을 weight로 삼아 histogram 만들어서 EMD
 
-    KL_div = np.sum(P*np.log(P/Q))
-    return KL_div
+    x_min = min(min(real_samples[:,index_x]), min(generated_samples[:,index_x]))
+    x_max = max(max(real_samples[:,index_x]), max(generated_samples[:,index_x]))
+    y_min = min(min(real_samples[:,index_y]), min(generated_samples[:,index_y]))
+    y_max = max(max(real_samples[:,index_y]), max(generated_samples[:,index_y]))
 
-def KL_with_KDE(generated_samples, real_samples):
-    # Fitting
-    kde_real = stats.gaussian_kde(real_samples.T)
-    kde_gen = stats.gaussian_kde(generated_samples.T)
+    position_cartesian = np.array(np.meshgrid(np.linspace(x_min, x_max, num_coordinate), np.linspace(y_min, y_max, num_coordinate))).T.reshape(-1,2)
+
+    real_samples_2d = real_samples[:,[index_x, index_y]]
+    generated_samples_2d = generated_samples[:,[index_x, index_y]]
+
+    kde_real = stats.gaussian_kde(real_samples_2d.T)
+    kde_gen = stats.gaussian_kde(generated_samples_2d.T)
+
     # Estimate distribution
-    density_real = kde_real(real_samples.T)
-    density_gen = kde_gen(generated_samples.T)
+    # density_real = kde_real(position_cartesian.T) #2by100 입력 -> 1by100 출력
+    # density_gen = kde_gen(position_cartesian.T)
+
+
+
+    # Estimate probability
+    interval_x = (x_max - x_min)/num_coordinate
+    interval_y = (y_max - y_min)/num_coordinate
+    interval = np.array([interval_x, interval_y])
+
+    integral_real = np.zeros((num_coordinate**2, 1))
+    integral_gen = np.zeros((num_coordinate**2, 1))
+    for i in range(num_coordinate**2):
+        integral_real[i] = kde_real.integrate_box(position_cartesian[i]-interval/2, position_cartesian[i]+interval/2)
+        integral_gen[i] = kde_gen.integrate_box(position_cartesian[i]-interval/2, position_cartesian[i]+interval/2)
+
+    #clipping
+    integral_real = np.maximum(integral_real,0)
+    integral_gen = np.maximum(integral_gen,0)
+
+
+    real_weight_position = np.concatenate((integral_real, position_cartesian),axis=1).astype('float32')
+    gen_weight_position = np.concatenate((integral_gen, position_cartesian),axis=1).astype('float32')
+
+
     # Compare
-    KL_div = KL(density_real, density_gen)
-    return KL_div
+    #KL_div = KL(density_real, density_gen)
+    #print('KL_div', KL_div)
 
-def KL_with_KDE_each_X(generated_samples, real_samples, num_in_cycle):
-    num_of_cycle = int(len(generated_samples)/num_in_cycle)
+    EMD_score, _, flow = cv2.EMD(real_weight_position, gen_weight_position, cv2.DIST_L2)
+
+    return EMD_score
+
+def EMD_from_1d_kde_integral(generated_samples, real_samples, index_x, num_coordinate): # 적분 weighting 버전
+
+    x_min = min(min(real_samples[:,index_x]), min(generated_samples[:,index_x]))
+    x_max = max(max(real_samples[:,index_x]), max(generated_samples[:,index_x]))
+
+
+    position_cartesian = np.linspace(x_min, x_max, num_coordinate).reshape(-1,1)
+
+    real_samples_1d = real_samples[:,[index_x]]
+    generated_samples_1d = generated_samples[:,[index_x]]
+
+    kde_real = stats.gaussian_kde(real_samples_1d.T)
+    kde_gen = stats.gaussian_kde(generated_samples_1d.T)
+
+    # Estimate distribution
+    #density_real = kde_real(position_cartesian.T)
+    #density_gen = kde_gen(position_cartesian.T)
+
+
+    # Estimate probability
+    interval = (x_max - x_min)/num_coordinate
+    integral_real = np.zeros((num_coordinate,1))
+    integral_gen = np.zeros((num_coordinate,1))
+    for i in range(num_coordinate):
+        integral_real[i] = kde_real.integrate_box_1d(position_cartesian[i]-interval/2, position_cartesian[i]+interval/2)
+        integral_gen[i] = kde_gen.integrate_box_1d(position_cartesian[i]-interval/2, position_cartesian[i]+interval/2)
+
+    #real_weight_position = np.concatenate((density_real.reshape(-1,1), position_cartesian),axis=1).astype('float32')
+    #gen_weight_position = np.concatenate((density_gen.reshape(-1,1), position_cartesian),axis=1).astype('float32')
+    real_weight_position = np.concatenate((integral_real, position_cartesian),axis=1).astype('float32')
+    gen_weight_position = np.concatenate((integral_gen, position_cartesian),axis=1).astype('float32')
+
+    # Compare
+    #KL_div = KL(density_real, density_gen)
+    #print('KL_div', KL_div)
+
+    EMD_score, _, flow = cv2.EMD(real_weight_position, gen_weight_position, cv2.DIST_L2)
+
+    return EMD_score
+
+def EMD_1d_2d_list_integral(normalized_generated_samples, normalized_real_samples, num_coordinate): #하나의 X에 대해, 모든 pair(36가지) EMD를 list로 뱉는 함수.
+
+    dim = normalized_real_samples.shape[1]
+    EMD_1d_score_list = []
+    EMD_2d_score_list = []
+
+    for i in range(dim):
+        for j in range(dim):
+            if i == j :
+                EMD_1d_score_list.append(EMD_from_1d_kde_integral(normalized_generated_samples, normalized_real_samples, i, num_coordinate))
+            elif i < j : 
+                EMD_2d_score_list.append(EMD_from_2d_kde_integral(normalized_generated_samples, normalized_real_samples, i, j, num_coordinate))
+
+    return np.array(EMD_1d_score_list), np.array(EMD_2d_score_list)
+
+
+def EMD_all_pair_each_X_integral(generated_samples, real_samples, num_coordinate, num_of_cycle, num_in_gen_list, num_in_real_list): #여러 X에 대해 각각 쪼개서, 모든 pair(36가지)에 대한 EMD list를 모아서 뱉는 함수.
+    dim = real_samples.shape[1]
     
-    KL_div_list = []
+    #EMD_score_list = []
+    EMD_1d_score_list = []
+    EMD_2d_score_list = []
+
     for i in range(num_of_cycle):
-        generated_samples_cycle = generated_samples[i*num_in_cycle : (i+1)*num_in_cycle]
-        real_samples_cycle = real_samples[i*num_in_cycle : (i+1)*num_in_cycle]
-        KL_div = KL_with_KDE(generated_samples_cycle, real_samples_cycle)
-        KL_div_list.append(KL_div)
+        generated_samples_cycle = generated_samples[sum(num_in_gen_list[:i]):sum(num_in_gen_list[:i])+num_in_gen_list[i]]
+        real_samples_cycle = real_samples[sum(num_in_real_list[:i]):sum(num_in_real_list[:i])+num_in_real_list[i]]
         
-    return np.mean(np.array(KL_div_list))
+        xy_mean = np.mean(real_samples_cycle, axis=0, dtype=np.float32)
+        xy_std = np.std(real_samples_cycle, axis=0, dtype=np.float32)
+        
+        normalized_generated_samples = (generated_samples_cycle-xy_mean)/xy_std
+        normalized_real_samples = (real_samples_cycle-xy_mean)/xy_std
+        
+        EMD_1d_score_list.append(EMD_1d_2d_list_integral(normalized_generated_samples, normalized_real_samples, num_coordinate)[0])
+        EMD_2d_score_list.append(EMD_1d_2d_list_integral(normalized_generated_samples, normalized_real_samples, num_coordinate)[1])
+        
+    return np.array(EMD_1d_score_list), np.array(EMD_2d_score_list)
 
-def EMD_each_X(generated_samples, real_samples, num_in_gen, num_in_cycle):
-    generated_samples = np.float32(generated_samples)
-    real_samples = np.float32(real_samples)
-    num_of_cycle = int(len(real_samples)/num_in_cycle)
-    EMD_score_list = []
-    for i in  range(num_of_cycle):
-        generated_samples_cycle = generated_samples[i*num_in_gen : (i+1)*num_in_gen]
-        real_samples_cycle = real_samples[i*num_in_cycle : (i+1)*num_in_cycle]
-        
-#         for i in range(len(generated_samples_cycle)):
-#             for j in range(len(generated_samples_cycle[i])):
-#                 if generated_samples_cycle[i][j] < 0:
-#                     print(generated_samples_cycle[i][j])
-        
-#         for i in range(len(real_samples_cycle)):
-#             for j in range(len(real_samples_cycle[i])):
-#                 if real_samples_cycle[i][j] < 0:
-#                     print(real_samples_cycle[i][j])
 
-        EMD, _, _ = cv2.EMD(generated_samples_cycle+100, real_samples_cycle+100, cv2.DIST_L2)
-        EMD_score_list.append(EMD)
-        
-    return np.mean(np.array(EMD_score_list)), EMD_score_list
-
-# EMD
-def EMD_from_2d_kde(generated_samples, real_samples, index_x, index_y, num_coordinate): #각 joint pair 별로 2d sample을 kde로 fittining시키고 얻은 pdf값을 weight로 삼아 histogram 만들어서 EMD
+def EMD_from_2d_kde_pdf(generated_samples, real_samples, index_x, index_y, num_coordinate): #각 joint pair 별로 2d sample을 kde로 fittining시키고 얻은 pdf값을 weight로 삼아 histogram 만들어서 EMD
 
     x_min = min(min(real_samples[:,index_x]), min(generated_samples[:,index_x]))
     x_max = max(max(real_samples[:,index_x]), max(generated_samples[:,index_x]))
@@ -144,7 +216,7 @@ def EMD_from_2d_kde(generated_samples, real_samples, index_x, index_y, num_coord
     return EMD_score
 
 
-def EMD_from_1d_kde(generated_samples, real_samples, index_x, num_coordinate): #대각 성분에 대해서는 1d kde로..
+def EMD_from_1d_kde_pdf(generated_samples, real_samples, index_x, num_coordinate): #대각 성분에 대해서는 1d kde로..
 
     x_min = min(min(real_samples[:,index_x]), min(generated_samples[:,index_x]))
     x_max = max(max(real_samples[:,index_x]), max(generated_samples[:,index_x]))
@@ -173,47 +245,28 @@ def EMD_from_1d_kde(generated_samples, real_samples, index_x, num_coordinate): #
     
     return EMD_score
 
-def EMD_all_pair(normalized_generated_samples, normalized_real_samples, num_coordinate): #하나의 X에 대해, 모든 pair(36가지) EMD를 list로 뱉는 함수.
+def EMD_1d_2d_list_pdf(normalized_generated_samples, normalized_real_samples, num_coordinate): #하나의 X에 대해, 모든 pair(36가지) EMD를 list로 뱉는 함수.
 
     dim = normalized_real_samples.shape[1]
-    EMD_score_list = []
+    EMD_1d_score_list = []
+    EMD_2d_score_list = []
 
     for i in range(dim):
         for j in range(dim):
             if i == j :
-                EMD_score_list.append(EMD_from_1d_kde(normalized_generated_samples, normalized_real_samples, i, num_coordinate))
-            else : 
-                EMD_score_list.append(EMD_from_2d_kde(normalized_generated_samples, normalized_real_samples, i, j, num_coordinate))
+                EMD_1d_score_list.append(EMD_from_1d_kde_pdf(normalized_generated_samples, normalized_real_samples, i, num_coordinate))
+            elif i < j : 
+                EMD_2d_score_list.append(EMD_from_2d_kde_pdf(normalized_generated_samples, normalized_real_samples, i, j, num_coordinate))
 
-    return np.array(EMD_score_list)
+    return np.array(EMD_1d_score_list), np.array(EMD_2d_score_list)
 
-def EMD_all_pair_each_X_val(generated_samples, real_samples, num_coordinate, num_in_gen, num_in_real): #여러 X에 대해 각각 쪼개서, 모든 pair(36가지)에 대한 EMD list를 모아서 뱉는 함수.
-    num_of_cycle = int(len(real_samples)/num_in_real)
-    
-    print(num_of_cycle)
+
+def EMD_all_pair_each_X_pdf(generated_samples, real_samples, num_coordinate, num_of_cycle, num_in_gen_list, num_in_real_list): #여러 X에 대해 각각 쪼개서, 모든 pair(36가지)에 대한 EMD list를 모아서 뱉는 함수.
     dim = real_samples.shape[1]
     
-    EMD_score_list = []
-    
-
-    for i in range(num_of_cycle):
-        generated_samples_cycle = generated_samples[i*num_in_gen : (i+1)*num_in_gen]
-        real_samples_cycle = real_samples[i*num_in_real : (i+1)*num_in_real]
-        
-        xy_mean = np.mean(real_samples_cycle, axis=0, dtype=np.float32)
-        xy_std = np.std(real_samples_cycle, axis=0, dtype=np.float32)
-        
-        normalized_generated_samples = (generated_samples_cycle-xy_mean)/xy_std
-        normalized_real_samples = (real_samples_cycle-xy_mean)/xy_std
-        
-        EMD_score_list.append(EMD_all_pair(normalized_generated_samples, normalized_real_samples, num_coordinate))
-        
-    return np.array(EMD_score_list)
-
-def EMD_all_pair_each_X_test(generated_samples, real_samples, num_coordinate, num_of_cycle, num_in_gen_list, num_in_real_list): #여러 X에 대해 각각 쪼개서, 모든 pair(36가지)에 대한 EMD list를 모아서 뱉는 함수.
-    dim = real_samples.shape[1]
-    
-    EMD_score_list = []
+    #EMD_score_list = []
+    EMD_1d_score_list = []
+    EMD_2d_score_list = []
 
     for i in range(num_of_cycle):
         generated_samples_cycle = generated_samples[sum(num_in_gen_list[:i]):sum(num_in_gen_list[:i])+num_in_gen_list[i]]
@@ -225,34 +278,7 @@ def EMD_all_pair_each_X_test(generated_samples, real_samples, num_coordinate, nu
         normalized_generated_samples = (generated_samples_cycle-xy_mean)/xy_std
         normalized_real_samples = (real_samples_cycle-xy_mean)/xy_std
         
-        EMD_score_list.append(EMD_all_pair(normalized_generated_samples, normalized_real_samples, num_coordinate))
+        EMD_1d_score_list.append(EMD_1d_2d_list_pdf(normalized_generated_samples, normalized_real_samples, num_coordinate)[0])
+        EMD_2d_score_list.append(EMD_1d_2d_list_pdf(normalized_generated_samples, normalized_real_samples, num_coordinate)[1])
         
-    return np.array(EMD_score_list)
-
-# def EMD_all_pair_each_X_val(generated_samples, real_samples, num_coordinate, num_of_cycle, num_in_gen_list, num_in_real_list): #여러 X에 대해 각각 쪼개서, 모든 pair(36가지)에 대한 EMD list를 모아서 뱉는 함수.
-#     dim = real_samples.shape[1]
-    
-#     sample_num = int(len(generated_samples)/num_of_cycle)
-#     num_in_cycle = int(len(real_samples)/num_of_cycle)
-    
-    
-#     EMD_score_list = []
-
-#     for i in range(num_of_cycle):
-#         generated_samples_cycle = generated_samples[i*sample_num:(i+1)*sample_num]
-#         real_samples_cycle = real_samples[i*num_in_cycle:(i+1)*num_in_cycle]
-        
-#         xy_mean = np.mean(real_samples_cycle, axis=0, dtype=np.float32)
-#         xy_std = np.std(real_samples_cycle, axis=0, dtype=np.float32)
-        
-#         normalized_generated_samples = (generated_samples_cycle-xy_mean)/xy_std
-#         normalized_real_samples = (real_samples_cycle-xy_mean)/xy_std
-        
-#         EMD_score_list.append(EMD_all_pair(normalized_generated_samples, normalized_real_samples, num_coordinate))
-        
-#     return np.array(EMD_score_list)
-
-    
-        
-
-    
+    return np.array(EMD_1d_score_list), np.array(EMD_2d_score_list)
